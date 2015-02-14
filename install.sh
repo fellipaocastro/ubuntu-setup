271950290f7aaf89f4f3a7ab3784b96daba9b944
Fellipe Castro <contact@fellipecastro.com>
2015-02-14 15:12:11 -0200

    Update repository

--- install.sh
+++ install.sh
@@ -5,11 +5,13 @@ sudo locale-gen UTF-8
 
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt-get autoremove -y
-sudo apt-get clean -y
 
-sudo apt-get install python-dev python-setuptools python3-setuptools ipython ipython3 \
-tree git exuberant-ctags supervisor nginx postgresql postgresql-contrib golang build-essential -y
+sudo apt-get install build-essential python-dev python-setuptools python3-setuptools ipython \
+ipython3 tree git exuberant-ctags supervisor nginx postgresql postgresql-contrib golang \
+redis-server mongodb-org -y
+
+git config --global push.default simple
+git config --global core.editor vim
 
 sudo easy_install -U pip
 
@@ -24,25 +26,24 @@ echo -e '\nsource /usr/local/bin/virtualenvwrapper.sh' >> ~/.bashrc
 
 sudo pip3 install flake8 ipdb httpie argparse
 
-git config --global push.default simple
-git config --global core.editor vim
-
 git clone https://github.com/rafael84/vim-ide.git
 source ~/vim-ide/install.sh
 touch ~/.vimrc_extra
 
 git clone https://github.com/powerline/fonts.git
-./fonts/install.sh
+~/fonts/install.sh
 git clone https://github.com/milkbikis/powerline-shell.git
-cp powerline-shell/config.py{.dist,}
-cd powerline-shell/ && ./install.py && cd ..
+cp ~/powerline-shell/config.py{.dist,}
+cd ~/powerline-shell/ && ./install.py && cd ~
 ln -s ~/powerline-shell/powerline-shell.py ~/powerline-shell.py
-echo -e '\nfunction _update_ps1() {\n    export PS1="$(~/powerline-shell.py $? 2> /dev/null)"\n}' >> ~/.bashrc
+echo -e '\nfunction _update_ps1() {\n    export PS1="$(~/powerline-shell.py $? 2> /dev/null)"\n}' \
+    >> ~/.bashrc
 echo -e '\nexport PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"' >> ~/.bashrc
 echo -e 'alias python2="python"' >> ~/.bash_aliases
 sed -i '1s/.*/#!\/usr\/bin\/env python2/' ~/powerline-shell/powerline-shell.py
 
 git clone https://github.com/fellipecastro/ubuntu-updater.git
-cp ubuntu-updater/updater.sh ~
+cp ~/ubuntu-updater/updater.sh ~
 
-touch ~/.bashrc
+sudo apt-get autoremove -y
+sudo apt-get clean -y
