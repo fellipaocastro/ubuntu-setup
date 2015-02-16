b9908399c53f5bbae089fddf074b6949cb485eed
Fellipe Castro <contact@fellipecastro.com>
2015-02-16 11:23:46 -0200

    Update repository

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-echo 'America/Sao_Paulo' > /etc/timezone
+sudo locale-gen UTF-8
+
+sudo ufw allow ssh
+sudo ufw allow www
+sudo ufw enable
 
 sudo fallocate -l 1G /swapfile
 sudo chmod 600 /swapfile
@@ -7,27 +11,30 @@ sudo mkswap /swapfile
 sudo swapon /swapfile
 sudo sh -c 'echo "/swapfile none swap sw 0 0" >> /etc/fstab'
 
-sudo locale-gen UTF-8
+echo 'America/Sao_Paulo' > /etc/timezone
+
+ssh-keygen -f /root/.ssh/id_rsa -N ''
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee \
     /etc/apt/sources.list.d/mongodb.list
 
-sudo apt-get update -y
-sudo apt-get upgrade -y
+sudo aptitude update -y
+sudo aptitude safe-upgrade -y
 
-sudo apt-get install build-essential python-dev python3-dev python-setuptools python3-setuptools \
+sudo aptitude install build-essential python-dev python3-dev python-setuptools python3-setuptools \
     ipython ipython3 tree git exuberant-ctags supervisor nginx postgresql postgresql-contrib \
     golang redis-server mongodb-org tig python-pip python3-pip ntp -y
+
+sudo aptitude clean -y
 
 git config --global push.default simple
 git config --global core.editor vim
 
 sudo pip2 install virtualenv virtualenvwrapper flake8 ipdb httpie argparse
+sudo pip3 install flake8 ipdb httpie argparse
 
 echo -e '\nsource /usr/local/bin/virtualenvwrapper.sh' >> ~/.bashrc
-
-sudo pip3 install flake8 ipdb httpie argparse
 
 git clone https://github.com/rafael84/vim-ide.git ~/vim-ide
 source ~/vim-ide/install.sh
@@ -47,8 +54,5 @@ sed -i '1s/.*/#!\/usr\/bin\/env python2/' ~/powerline-shell/powerline-shell.py
 
 git clone https://github.com/fellipecastro/ubuntu-updater.git ~/ubuntu-updater
 cp ~/ubuntu-updater/updater.sh ~
-
-sudo apt-get autoremove -y
-sudo apt-get clean -y
 
 sudo reboot
