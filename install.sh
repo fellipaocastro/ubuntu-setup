24445ce5bdf6aa821e239ea4954c8cb80d232b0a
Fellipe Castro <contact@fellipecastro.com>
2015-12-27 13:25:59 -0200

    Update

--- install.sh
+++ install.sh
@@ -59,6 +59,7 @@ sudo aptitude clean -y
 
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable --ruby
+source /usr/local/rvm/scripts/rvm
 rvm --default use current
 
 sudo pip2 install virtualenv virtualenvwrapper flake8 ipdb httpie argparse
