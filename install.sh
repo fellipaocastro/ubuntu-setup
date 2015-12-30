e8ebe9acae86c2ea02bba3dd8e74259ea99d447a
Fellipe Castro <contact@fellipecastro.com>
2015-12-30 17:05:32 -0200

    updater.sh

--- install.sh
+++ install.sh
@@ -33,7 +33,7 @@ sed -i.bak 's~\(ZSH_THEME="\)[^"]*\(".*\)~\1dracula\2~' ~/.zshrc
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 sudo echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | \
     sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
-    
+
 curl https://repo.varnish-cache.org/GPG-key.txt | sudo apt-key add -
 echo "deb https://repo.varnish-cache.org/ubuntu/ trusty varnish-4.1" >> \
     /etc/apt/sources.list.d/varnish-cache.list
