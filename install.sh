99d6ba1912234baa5a22439a91bf5e6e08868f1e
Fellipe Castro <contact@fellipecastro.com>
2016-09-02 13:39:17 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ sudo ufw allow www
 sudo yes 'yes' | ufw enable
 
 sudo fallocate -l 1G /swapfile
-sudo chmod 600 /swapfiledr
+sudo chmod 600 /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile
 sudo sh -c 'echo "/swapfile none swap sw 0 0" >> /etc/fstab'
